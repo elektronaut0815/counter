8ba32ffe39bd756cd408843c5ae3bcdf5317dcb8
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2024-06-21 14:52:49 +0200

    Updating the test.

--- tests/counter.ts
+++ tests/counter.ts
@@ -1,16 +1,38 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Counter } from "../target/types/counter";
+import * as anchor from '@coral-xyz/anchor';
+import { Keypair } from '@solana/web3.js';
+import { assert } from 'chai';
 
-describe("counter", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('counter', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  // Unfortunately, the following line does not work as expected.
+  //const program = anchor.workspace.Counter as Program<Counter>;
+  // Instead, we need to manually read the IDL and generate the program client.
+  // Read the generated IDL.
+  const idl = JSON.parse(require('fs').readFileSync('./target/idl/counter.json', 'utf8'));
+  //Address of the deployed program - TODO: Update this with the deployed program address
+  const programId = new anchor.web3.PublicKey('CoA6pQ7J8K4YVWLtk2EDapNb4wF73G3FXcVXdrnYVVQU');
+  if (!programId) {
+    throw new Error('Counter programId is not initialized properly');
+  }
+  //Generate the program client from IDL
+  const program = new anchor.Program(idl, programId);
+  if (!program) {
+    throw new Error('Counter program is not initialized properly');
+  }
 
-  const program = anchor.workspace.Counter as Program<Counter>;
+  const counterAccount = new Keypair();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('Is initialized!', async () => {
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        counter: counterAccount.publicKey,
+      })
+      .signers([counterAccount])
+      .rpc({ skipPreflight: true });
+    console.log('Your transaction signature', tx);
+    const accountData = await program.account.counter.fetch(counterAccount.publicKey);
+    assert.equal(accountData.count.toString(), '0');
   });
 });
