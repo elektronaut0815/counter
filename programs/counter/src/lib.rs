d5395fa77f228a5106015163da9b6f6539ecb405
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2024-06-21 14:52:29 +0200

    Updating the program address.

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("zK96WPaENqkNzFMSSwUWYrjmPLFTsHUL8sp3aKaYBTF");
+declare_id!("CoA6pQ7J8K4YVWLtk2EDapNb4wF73G3FXcVXdrnYVVQU");
 
 #[program]
 pub mod counter {
@@ -14,7 +14,7 @@ pub mod counter {
 
     pub fn increment(ctx: Context<Increment>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
-        counter.count += 1;
+        counter.count.checked_add(1).unwrap();
         Ok(())
     }
 }
